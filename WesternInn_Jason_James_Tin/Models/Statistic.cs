89ca3cd391182fa9e0dad7bcde35c867bd97f3ca
Trung Tin Phan <19996136@student.westernsydney.edu.au>
2022-10-27 12:37:47 +1100

    Update on 7.2 27/10

--- WesternInn_Jason_James_Tin/Models/Statistic.cs
+++ WesternInn_Jason_James_Tin/Models/Statistic.cs
@@ -1,6 +1,18 @@
-﻿namespace WesternInn_Jason_James_Tin.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WesternInn_Jason_James_Tin.Models
 {
     public class Statistic
     {
+        [Display(Name = "Postcode")]
+        public string PostCode { get; set; } = string.Empty;
+        [Display(Name = "Number of Guests")]
+        public int GuestCount { get; set; }
+
+        [Display(Name = "Room ID")]
+        public int RoomID { get; set; }
+        [Display(Name = "Number of Bookings")]
+        public int BookingCount { get; set; }
     }
 }
--- WesternInn_Jason_James_Tin/Pages/Statistics/Statistics.cshtml.cs
+++ WesternInn_Jason_James_Tin/Pages/Statistics/Statistics.cshtml.cs
@@ -1,15 +1,41 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
+using WesternInn_Jason_James_Tin.Models;
 
 namespace WesternInn_Jason_James_Tin.Pages.Statistics
 {
     [Authorize(Roles = "administrators")]
     public class StatisticsModel : PageModel
     {
-        public void OnGet()
+        private readonly WesternInn_Jason_James_Tin.Data.ApplicationDbContext _context;
+
+        public StatisticsModel(WesternInn_Jason_James_Tin.Data.ApplicationDbContext context)
         {
+            _context = context;
         }
+        public IList<Statistic> PostcodeStats { get; set; } = default!;
+        public IList<Statistic> RoomStats { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var postcodeGroups = _context.Guest.GroupBy(m => m.PostCode);
+
+            PostcodeStats = await postcodeGroups
+                             .Select(g => new Statistic { PostCode = g.Key, GuestCount = g.Count() })
+                             .ToListAsync();
+
+            var roomGroup = _context.Booking.GroupBy(m => m.RoomID);
+
+            RoomStats = await roomGroup
+                             .Select(g => new Statistic { RoomID = g.Key, BookingCount = g.Count() })
+                             .ToListAsync();
+            return Page();
+        }
+        /*public void OnGet()
+        {
+        }*/
     }
 }
